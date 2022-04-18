02a9bae93d945620136c42e8d1ecbe957c6a64b4
minjae <guswlfahs123@naver.com>
2022-04-18 13:49:29 +0900

    navigator

---
+++ lib/navigator/screen/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:code_factory/navigator/screen/route_one_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../layout/main_layout.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        // true - pop 가능
+        // false - pop 불가능
+        // 작업실행
+        final canPop = Navigator.of(context).canPop();
+
+        return canPop;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print(Navigator.of(context).canPop());
+            },
+            child: Text('Can Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop();
+            },
+            child: Text('Maybe Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
+                ),
+              );
+
+              print(result);
+            },
+            child: Text('Push'),
+          ),
+        ],
+      ),
+    );
+  }
+}
